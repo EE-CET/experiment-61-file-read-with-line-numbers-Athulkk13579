ab3faad5799ffe1051e20a3e51a4afff52c28f20
Athulkk13579 <athulkrishnak8086@gmail.com>
2026-03-27 15:48:59 +0530

    Update FileLineReader.java

--- src/FileLineReader.java
+++ src/FileLineReader.java
@@ -1,12 +1,14 @@
+
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 
-public class FileReadWithLineNumbers {
+public class FileLineReader {
     public static void main(String[] args) {
         String fileName = "input.txt";
 
         try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
+            
             String line;
             int lineNumber = 1;
 
@@ -14,8 +16,9 @@ public class FileReadWithLineNumbers {
                 System.out.println(lineNumber + ": " + line);
                 lineNumber++;
             }
+
         } catch (IOException e) {
-            System.out.println("An error occurred while reading the file: " + e.getMessage());
+            System.out.println("Error reading file: " + e.getMessage());
         }
     }
 }
